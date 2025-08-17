a4aba0e5d5ead84901dde7bbc62b054454d5f585
~L4u <laudivan@live.com>
2025-08-17 15:12:06 -0300

    setting my toolbox environment

--- toolbox/environment/toolbox-env-init.sh
+++ toolbox/environment/toolbox-env-init.sh
@@ -9,11 +9,13 @@ done < $CONTAINERENV
 
 PACKAGES=""
 
-[[ ! -s /usr/bin/podman-remote ]] && PACKAGES+=' podman-remote'
+[[ ! -s /usr/bin/podman-remote ]] && PACKAGES+=" podman-remote.x86_64"
+[[ ! -s /usr/bin/host-spawn ]] && PACKAGES+=" host-spawn.x86_64"
+[[ ! -s /usr/bin/zsh ]] && PACKAGES+=" zsh.x86_64 zsh-autosuggestions.noarch"
 
-[[ $PACKAGES!="" ]] && \
+[[ ! -z "$PACKAGES" ]] && \
     dnf check-update && \
-    sudo dnf --assumeyes install $PACKAGES && \
+    sudo dnf install -y --skip-unavailable "$PACKAGES" && \
     sudo dnf clean all
 
 alias podman="/usr/bin/podman-remote"
