cd7e5b191097f56abe046e3e98b01710f4c6b328
~L4u <laudivan@live.com>
2025-08-27 21:28:25 -0300

    Toolbox init

--- toolbox/environment/toolbox-env-init.sh
+++ toolbox/environment/toolbox-env-init.sh
@@ -7,30 +7,6 @@ while read LINE; do
     export $KEY="$(echo $LINE | cut -d '=' -f 2)"
 done < $CONTAINERENV
 
-function podman {
-    CONTAINER_HOST=unix:///run/user/$UID/podman/podman.sock
-
-    [[ ! -f /usr/bin/podman-remote ]] && \
-        sudo dnf --refresh --color=always --assumeyes \
-	        --quiet --best \
-            install podman-remote.x86_64 && \
-        sudo dnf clean all
-
-    podman-remote --url $CONTAINER_HOST "$*"
-
-    return $?
-}
-
-function flatpak {
-    [[ ! -f /usr/bin/flatpak-spawn ]] && \
-        sudo dnf --refresh install --assumeyes flatpak-spawn.x86_64 && \
-        sudo dnf clean all
-
-    flatpak-spawn --host flatpak "$*"
-
-    return $?
-}
-
 function desktopUpdate {
 	ID=${TOOLBOX_ID//\"/}
 	DIR_ICONS=/usr/share/icons/hicolor/scalable/apps
@@ -41,18 +17,47 @@ function desktopUpdate {
 	[[ ! -d $DEST_DIR_ICONS ]] && mkdir -p $DEST_DIR_ICONS
 
 	for LAUNCHER in /usr/share/applications/${1}*.desktop; do
-		AUX=$(basename ${LAUNCHER})
+        AUX=$(basename ${LAUNCHER})
 		DESTINATION=~/.local/share/applications/toolbox-${ID}@${AUX%%.desktop}.desktop
 
 		cat ${LAUNCHER} | \
 			sed "s/Exec=/Exec=toolbox run --container=$TOOLBOX_NAME /g" | \
 			sed "s/Icon=/Icon=${ID}@/g" > ${DESTINATION}
-		cp -u $DIR_ICONS/${AUX%%.desktop}.svg $DEST_DIR_ICONS/${ID}@${AUX%%.desktop}.svg
+		cp -u $DIR_ICONS/${AUX%%.desktop}.svg \
+		    $DEST_DIR_ICONS/${ID}@${AUX%%.desktop}.svg
 	done
 }
 
+export CONTAINER_HOST=unix:///run/user/$UID/podman/podman.sock
+[[ ! -f /usr/bin/podman-remote ]] && \
+    sudo dnf --quiet --refresh --assumeyes install podman-remote.x86_64 &&
+    alias podman="podman-remote --url $CONTAINER_HOST"
+
+[[ ! -f /usr/bin/flatpak-spawn ]] && \
+    sudo dnf --quiet --refresh --assumeyes install flatpak-spawn.x86_64 && \
+    alias flatpak="flatpak-spawn"
+
+[[ ! -f /usr/bin/host-spawn ]] && \
+    sudo dnf --quiet --refresh --assumeyes install host-spawn
+
 [[ ! -d /var/home/linuxbrew ]] && \
-    sudo dnf --refresh --assumeyes install gcc && \
+    sudo dnf --quiet --refresh --assumeyes install gcc && \
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+[[ -d /var/home/linuxbrew/.linuxbrew/bin/brew ]] &&
+    eval $(/var/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    
+[[ ! -f /usr/bin/xdg-settings ]] && \
+    sudo sudo dnf --quiet --refresh --assumeyes install xdg-utils
+
+[[ ! -f ~/.local/share/applications/flatpak-spawn@com.google.Chrome.desktop ]] && \
+    xdg-settings set default-web-browser flatpak-spawn@com.google.Chrome.desktop 
+    
+[[ ! -f /usr/bin/zsh ]] && \
+    sudo dnf --quiet --refresh --assumeyes install zsh
 
 #export PROMPT="%{%f%b%k%} ~L4u $TOOLBOX_NAME  $(build_prompt)"
+
+export HOSTNAME="${HOSTNAME}-${TOOLBOX_NAME//\"/}"
+
+alias dnf="sudo dnf --assumeyes"
