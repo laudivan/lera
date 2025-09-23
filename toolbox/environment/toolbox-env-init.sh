41dfff1723bac13477cf28829e434051eb00affe
~L4u <laudivan@live.com>
2025-09-23 18:01:45 -0300

    ...

--- toolbox/environment/toolbox-env-init.sh
+++ toolbox/environment/toolbox-env-init.sh
@@ -8,6 +8,10 @@ function code {
     fi
 }
 
+[[ -v ZSH  ]] && {
+	export ZSH_THEME=$([[ -s $CONTAINERENV ]] && echo "crunch" || echo "agnoster")
+	source ~/.oh-my-zsh/oh-my-zsh.sh
+}
 
 [[ ! -s $CONTAINERENV ]] && return
 
@@ -15,6 +19,11 @@ while read LINE; do
     KEY="TOOLBOX_$(echo $LINE | cut -d '=' -f 1 | tr '[:lower:]' '[:upper:]')"
     export $KEY="$(echo $LINE | cut -d '=' -f 2)"
 done < $CONTAINERENV
+
+function dnf {
+    sudo dnf --refresh --assumeyes "$@"
+    sudo dnf clean all
+}
 
 function desktopUpdate {
 	ID=${TOOLBOX_ID//\"/}
@@ -37,42 +46,44 @@ function desktopUpdate {
 	done
 }
 
+function podman {
+    podman-remote "$@"
+}
+
+[[ ! -f /usr/bin/pv ]] && dnf install pv
+[[ ! -f /usr/bin/zsh ]] && dnf install zsh && chsh --shell /usr/bin/zsh
 
 export CONTAINER_HOST=unix:///run/user/$UID/podman/podman.sock
 [[ ! -f /usr/bin/podman-remote ]] && \
-    sudo dnf --quiet --refresh --assumeyes install podman-remote.x86_64 && \
-    sudo dnf clean all
+    dnf install podman-remote.x86_64 && \
+    dnf clean all
 
 alias podman="podman-remote --url $CONTAINER_HOST"
 
 [[ ! -f /usr/bin/flatpak-spawn ]] && \
-    sudo dnf --quiet --refresh --assumeyes install flatpak-spawn.x86_64 && \
-    sudo dnf clean all
+    dnf install flatpak-spawn.x86_64 && \
+    dnf clean all
 
 [[ ! -f /usr/bin/host-spawn ]] && \
-    sudo dnf --quiet --refresh --assumeyes install host-spawn && \
-    sudo dnf clean all
+    dnf install host-spawn && \
+    dnf clean all
 
 alias flatpak="host-spawn flatpak"
 
 [[ ! -d /var/home/linuxbrew ]] && \
-    sudo dnf --quiet --refresh --assumeyes install gcc && \
+    dnf install gcc && \
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
     eval $(/var/home/linuxbrew/.linuxbrew/bin/brew shellenv) && \
-    brew install zsh kakoune
+    brew install kakoune fastfetch onefetch rich-cli fzf direnv
 
 [[ -s /var/home/linuxbrew/.linuxbrew/bin/brew ]] &&
     eval $(/var/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 [[ ! -f /usr/bin/xdg-settings ]] && \
-    sudo sudo dnf --quiet --refresh --assumeyes install xdg-utils
+    dnf install xdg-utils
 
 [[ ! -f ~/.local/share/applications/flatpak-spawn@com.google.Chrome.desktop ]] && \
     xdg-settings set default-web-browser flatpak-spawn@com.google.Chrome.desktop
 
 export HOSTNAME="${HOSTNAME}-${TOOLBOX_NAME//\"/}"
 
-function dnf {
-    sudo dnf --quiet --refresh --assumeyes "$@"
-    sudo dnf clean all
-}
